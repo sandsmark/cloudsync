0f85dc1e6c5931327f6698501441a44b530be1a1
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2010-03-28 05:35:01 +0200

    ignore build dir

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sandsmark/prosjekter/cloudsync
-# Build directory: /home/sandsmark/prosjekter/cloudsync/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(doc)
-SUBDIRS(src)
-SUBDIRS(icons)
